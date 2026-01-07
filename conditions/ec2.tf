78c7fdd7ab0747363024d52abddd364c64986ff7
Wasim <imwasim.shaik@gmail.com>
2026-01-07 12:02:23 +0530

    committed terraform conditions

---
+++ conditions/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "terraform"{
+    ami = var.ami_id
+    instance_type = var.env == "dev" ? var.instance_type : "t3.small"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = "terraform"
+        Terraform = "true"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+    name = allow_all
+
+    egress {
+        from_port = 0 # from port 0 to port 0 means all ports
+        to_port = 0
+        protocol = -1 # means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
+    }
+
+    ingress {
+        from_port = 0 # from port 0 to port 0 means all ports
+        to_port = 0
+        protocol = -1 # means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
+    }
+    tags {
+        Name = "allow-all"
+    }
+}
