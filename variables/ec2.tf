1061ffacf3300edf13169d1bffe0762e1cf7615d
Wasim <imwasim.shaik@gmail.com>
2026-01-08 13:55:22 +0530

    terraform variables

---
+++ variables/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "terraform"{
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = "terraform"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+    name = "allow_all"
+     egress {
+        from_port = 0 # from port 0 to port 0 means all ports
+        to_port = 0
+        protocol = -1 # means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
+    }
+
+    ingress {
+        from_port = 0 # from port 0 to port 0 means all ports
+        to_port = 0
+        protocol = -1 # means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
+    }
+    tags = {
+        Name = "allow-all"
+    }
+  
+}
---
+++ variables/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+  type = string
+  default = "ami-12345"
+}
+
+variable "instance_type"{
+    type = string
+   
+}
